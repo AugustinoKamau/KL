fe7be224b21a78d64b77dbd170c61098725b6aa5
Kamau Augustino <kpa403@usask.ca>
2026-05-06 01:16:06 -0600

    I have improved my fprint to fprintf.

--- main2.c
+++ main2.c
@@ -1,59 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void){
-	int num1;
-	int num2;
-	double sum;
-	double result;
-	char OP;
+int main(void) {
+    int num1, num2;
+    char OP;
+    double result;
 
-	printf("Enter number: \n");
-	if (scanf("%d %d", &num1, &num2) != 2){
-		fprint(stderr, "Invalid input.\n");
-	return EXIT_FAILURE;
-	}
+    printf("Enter two numbers: ");
+    if (scanf("%d %d", &num1, &num2) != 2) {
+        fprintf(stderr, "Invalid input.\n");
+        return EXIT_FAILURE;
+    }
 
-	printf("choose the +, -, *, /,% %.\n");
-	if (scanf("%c", &OP) != 1){
-		fprint(stderr, "Invalid");
-		return EXIT_FAILURE;
-	}
+    printf("Choose operator (+, -, *, /, %%): ");
+    if (scanf(" %c", &OP) != 1) {
+        fprintf(stderr, "Invalid operator input.\n");
+        return EXIT_FAILURE;
+    }
 
-	if (OP == '+'){
-		sum = num1 + num2;
-		printf("The sum = %.2f\n", sum);
-	}
+    if (OP == '+') {
+        result = num1 + num2;
+        printf("The sum = %.2f\n", result);
+    } else if (OP == '-') {
+        result = num1 - num2;
+        printf("The difference = %.2f\n", result);
+    } else if (OP == '*') {
+        result = num1 * num2;
+        printf("The product = %.2f\n", result);
+    } else if (OP == '/') {
+        if (num2 == 0) {
+            fprintf(stderr, "Cannot divide by zero.\n");
+            return EXIT_FAILURE;
+        }
+        result = (double)num1 / num2;
+        printf("The result = %.2f\n", result);
+    } else if (OP == '%') {
+        if (num2 == 0) {
+            fprintf(stderr, "Cannot modulo by zero.\n");
+            return EXIT_FAILURE;
+        }
+        int rem = num1 % num2;
+        printf("The remainder = %d\n", rem);
+    } else {
+        fprintf(stderr, "Invalid operator.\n");
+        return EXIT_FAILURE;
+    }
 
-	else if (OP == '-'){
-		sum = num1 - num2;
-		printf("The difference = %.2f\n", sum);
-	}
-
-	else if (OP == '*'){
-                 sum = num1 * num2;
-		printf("The product = %.2f\n", sum);
-	}
-
-	else if (OP == '/') {
-		if (num2 == 0){
-			fprintf(stderr, "Invalid it is not divisible by 0");
-			return EXIT_FAILURE;
-		}
-
-		else{
-			result = num1 / num2;
-			printf("The result = %.2f",result);
-		}
-
-	else if (OP == %){
-                        int rem = num1 % num2;
-                        printf("The remainder = %d",rem);
-                }
-
-	else{
-		fprintf(stderr,"Invalid Operator");
-		return EXIT_FAILURE;
-	}
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
+
