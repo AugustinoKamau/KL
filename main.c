d184be188282ccaffc904e57c8db86b9803ba093
Kamau Augustino <kpa403@usask.ca>
2026-05-05 23:17:51 -0600

    I have completed day 1 and 2

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+        int x;
+        int y;
+        scanf("%d",&x);
+        scanf("%d", &y);
+        int sum = y + x;
+        printf("The sum of x + y = %d", sum);
+        return EXIT_SUCCESS;
+}
