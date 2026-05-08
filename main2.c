a8cfca137c7312cdd120286d89dc598d74f098ce
Kamau Augustino <kpa403@usask.ca>
2026-05-08 01:07:16 -0600

    I have made my calculate using controlflow switch instead of if statements

--- main2.c
+++ main2.c
@@ -2,50 +2,53 @@
 #include <stdlib.h>
 
 int main(void) {
-    int num1, num2;
-    char OP;
-    double result;
+	int num1, num2;
+    	char OP;
+    	double result;
 
-    printf("Enter two numbers: ");
-    if (scanf("%d %d", &num1, &num2) != 2) {
-        fprintf(stderr, "Invalid input.\n");
-        return EXIT_FAILURE;
-    }
+    	printf("Enter two numbers: ");
+    	if (scanf("%d %d", &num1, &num2) != 2) {
+        	fprintf(stderr, "Invalid input.\n");
+        	return EXIT_FAILURE;
+    	}	
 
-    printf("Choose operator (+, -, *, /, %%): ");
-    if (scanf(" %c", &OP) != 1) {
-        fprintf(stderr, "Invalid operator input.\n");
-        return EXIT_FAILURE;
-    }
-
-    if (OP == '+') {
-        result = num1 + num2;
-        printf("The sum = %.2f\n", result);
-    } else if (OP == '-') {
-        result = num1 - num2;
-        printf("The difference = %.2f\n", result);
-    } else if (OP == '*') {
-        result = num1 * num2;
-        printf("The product = %.2f\n", result);
-    } else if (OP == '/') {
-        if (num2 == 0) {
-            fprintf(stderr, "Cannot divide by zero.\n");
-            return EXIT_FAILURE;
-        }
-        result = (double)num1 / num2;
-        printf("The result = %.2f\n", result);
-    } else if (OP == '%') {
-        if (num2 == 0) {
-            fprintf(stderr, "Cannot modulo by zero.\n");
-            return EXIT_FAILURE;
-        }
-        int rem = num1 % num2;
-        printf("The remainder = %d\n", rem);
-    } else {
-        fprintf(stderr, "Invalid operator.\n");
-        return EXIT_FAILURE;
-    }
-
-    return EXIT_SUCCESS;
+    	printf("Choose operator (+, -, *, /, %%): ");
+    	if (scanf(" %c", &OP) != 1) {
+        	fprintf(stderr, "Invalid operator input.\n");
+        	return EXIT_FAILURE;
+    	}
+	switch(OP){
+		case '+':
+			result = num1 + num2;
+			printf("The sum = %.2f", result);
+			break;
+		case '-':
+                        result = num1 - num2;
+                        printf("The difference = %.2f", result);
+                        break;
+		case '*':
+                        result = num1 * num2;
+                        printf("The product = %.2f", result);
+                        break;
+		case '/':
+			if (num1 == 0){
+				fprintf(stderr, "cannot divide by zero");
+				return EXIT_FAILURE;
+			}
+                        result = num1 / num2;
+                        printf("The difference = %.2f", result);
+                        break;
+		case '%':
+			if (num2 == 0) {
+                		fprintf(stderr, "Cannot modulo by zero.\n");
+                		return EXIT_FAILURE;
+            		}
+                        result = num1 %  num2;
+                        printf("The remainder = %.2f", result);
+                        break;
+		default:
+			printf("Invalid operation!!\n");
+	}
+    	return EXIT_SUCCESS;
 }
 
