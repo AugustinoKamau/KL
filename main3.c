ce682e647df4906b7e7d33419644e06c11b06b6c
Kamau Augustino <kpa403@usask.ca>
2026-05-08 02:00:39 -0600

    I have used the fuctions on my calculator and also use user input and used controlflow.

--- main3.c
+++ main3.c
@@ -8,13 +8,86 @@ int add(int a, int b)
 	return sum;
 }
 
+int sub(int a, int b)
+{
+	int dif;
+        dif = a - b;
+        return dif;
+}
+
+int mul(int a, int b)
+{
+	int product;
+        product = a * b;
+        return product;
+}
+
+int div_int(int a, int b)
+{
+	int division;
+        division= a / b;
+        return division;
+}
+
+int mod_int(int a, int b)
+{
+        int modula;
+        modula = a % b;
+        return modula;
+}
+
 int main(void)
 {
-	int result;
+	int a, b, result;
+	char OP;
 	
-	result = add(3,50);
-	printf("3 + 50 = %d", result);
- 	printf("\n");
-	printf("3 + 50 = %d", add(3, 50));
+	printf("Enter two numbers: ");
+    	if (scanf("%d %d", &a, &b) != 2) {
+        	fprintf(stderr, "Invalid input.\n");
+        	return EXIT_FAILURE;
+    	}	
+
+    	printf("Choose operator (+, -, *, /, %%): ");
+    	if (scanf(" %c", &OP) != 1) {
+        	fprintf(stderr, "Invalid operator input.\n");
+        	return EXIT_FAILURE;
+    	}
+	switch(OP)
+	{
+		case '+':
+			result = add(a,b);
+			printf("The sum = %d", result);
+			break;
+	
+		case'-':
+			result = sub(a,b);
+			printf("The difference = %d", result);
+			break;
+
+		case'*':
+			result = mul(a,b);
+			printf("The product = %d", result);
+			break;
+		case'/':
+			if (b == 0) {
+                		fprintf(stderr, "Cannot divide by zero.\n");
+                		return EXIT_FAILURE;
+           		}
+            		result = div_int(a, b);
+            		printf("The division = %d\n", result);
+           		 break;
+		case'%':
+			if (b == 0) {
+                		fprintf(stderr, "Cannot modulo by zero.\n");
+                		return EXIT_FAILURE;
+            		}
+            		result = mod_int(a, b);
+            		printf("The remainder = %d\n", result);
+            		break;
+
+		default:
+			fprintf(stderr, "Invalid operation.\n");
+            		return EXIT_FAILURE;
+	}
 	return EXIT_SUCCESS;
 }
