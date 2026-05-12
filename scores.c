b104de022e9cd2bd3ece5cefa0d8b8543a7a9372
Kamau Augustino <kpa403@usask.ca>
2026-05-12 12:09:12 -0600

    I have added implemetation for the read_scores.

--- scores.c
+++ scores.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "scores.h"
 
 int read_scores(int arr[], int n)
 {
-}
+	if (n != 10)
+	{
+		fprintf(stderr, "Invalid input.\n");
+		return EXIT_FAILURE;
+	}
 
+	printf("Enter an Integer (0 -100).\n): ");
+	for (i=0; i < n; i++)
+	{
+		if (scanf("%d", &arr[i]) !=1)
+		{
+			fprintf(stderr, "Invalid input.\n");
+                	return EXIT_FAILURE;
+        	}
+	}
+	return EXIT_SUCCESS;
+
+}
+/*
 double calc_average(const int arr[], int n)
 {
 }
@@ -20,4 +38,4 @@ int find_min(int arr[], int n)
 int count_pass(const int arr[], int n)
 {
 }
-
+*/
