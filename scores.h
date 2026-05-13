2cfd2170a7e8534a5f3b8cf0e200b55233906121
Kamau Augustino <kpa403@usask.ca>
2026-05-12 21:20:10 -0600

    added lines on scores.h

--- scores.h
+++ scores.h
@@ -1,6 +1,11 @@
+#ifndef SCORES_H
+#define SCORES_H
+
 int read_scores(int arr[], int n);
 double calc_average(const int arr[], int n);
 int find_max(int arr[], int n);
 int find_min(int arr[], int n);
 int count_pass(const int arr[], int n);
 int main(void);
+
+#endif
